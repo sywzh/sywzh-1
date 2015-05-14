a7bca7fb85a1e041cdb92d53de0fba391394d152
wzh <wzh@ubuntu.(none)>
2015-05-14 21:33:21 +0800

    over1

--- demo/User/urls.py
+++ demo/User/urls.py
@@ -7,6 +7,6 @@ import views
 urlpatterns = patterns('',
     url(r'^login/$', views.LoginHd),
     url(r'^register/$',views.RegisterHd),
-    url(r'^/logout/$',views.LogoutHd),
-    url(r'^/changepw/$',views.ModifyPw)
+    url(r'^logout/$',views.LogoutHd),
+    url(r'^changepw/$',views.ModifyPw)
 )
